8bc19d2d4be094d5707e2b3cb26dc32047ba235e
zhangqingzheng <zhang.qingzheng@qq.com>
2016-06-05 07:20:16 -0400

    auto_install.sh: modify install error

--- auto_install.sh
+++ auto_install.sh
@@ -5,20 +5,36 @@ set -o xtrace
 
 ./init.sh
 ./create_link.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap03/mysql/localrc 
-chap03/mysql/mysql.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap03/rabbitmq/localrc
-chap03/rabbitmq/rabbitmq.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap03/keystone/localrc
-chap03/keystone/keystone.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap04/localrc
-chap04/swift.sh
-chap04/swift-storage.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap05/localrc
-chap05/glance-with-swift.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap06/localrc
-chap06/quantum.sh
-chap06/quantum-agent.sh
+pushd  chap03/mysql
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc 
+./mysql.sh
+popd
+pushd  chap03/rabbitmq
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./rabbitmq.sh
+popd
+pushd chap03/keystone
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+apt-get install zlib1g-dev
+apt-get upgrade python-oslo.config
+#安装openstack-ubuntu-14-04/packages/pip
+./keystone.sh
+popd
+pushd chap04
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+apt-get install nfs-common
+./swift.sh
+./swift-storage.sh
+popd
+pushd chap05
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./glance-with-swift.sh
+popd
+pushd chap06
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./quantum.sh
+./quantum-agent.sh
+popd
 cp -rf interface /etc/network/
 ifconfig eth3 down 
 ifconfig eth3 0.0.0.0 up
@@ -27,16 +43,19 @@ ovs-vsctl add-port br-ex eth3
 ifdown br-ex
 ifup br-ex
 sed -i '/#net.ipv4.ip_forward=1/s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf 
-
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap07/localrc
-chap07/cinder.sh
-chap07/cinder-volume.sh
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap08/allinone/localrc
-chap08/allinone/nova-api.sh
-chap08/allinone/nova-compute.sh
-
-sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' chap09/localrc
-chap09/dashboard.sh
-
+pushd chap07
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./cinder.sh
+./cinder-volume.sh
+popd
+pushd chap08/allinone
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./nova-api.sh
+./nova-compute.sh
+popd
+pushd chap09
+sed -i '/192.168.56.180/s/192.168.56.180/192.168.56.181/g' ./localrc
+./dashboard.sh
+popd
 
 set +o xtrace
