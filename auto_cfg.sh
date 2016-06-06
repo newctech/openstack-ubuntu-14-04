c737126b898fa0ac5006e09911b062a201887a83
zhangqingzheng <zhang.qingzheng@qq.com>
2016-06-06 10:30:44 -0400

    auto_cfg.sh: modify

--- auto_cfg.sh
+++ auto_cfg.sh
@@ -9,9 +9,11 @@ cp cinderrc adminrc
 sed -i 's,service,admin,g' ./adminrc
 sed -i 's,keystone_cinder_password,admin_user_password,g' ./adminrc
 sed -i 's,cinder,admin,g' ./adminrc
-
+sleep 3
 source adminrc
 
 popd
+
+cp /usr/local/bin/quantum /usr/local/bin/neutron
 
 set +o xtrace
